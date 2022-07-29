9e016e04b369e85445a0a99bd0be7a4327cd48bc
Barış Can Çifçi <78167969+Bariscncfc@users.noreply.github.com>
2022-07-29 18:24:39 +0300

    [feat] Finish
    finish the project

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import { useState } from "react";
 import { useToast } from "@chakra-ui/react";
 import RangeSlider from "./components/RangeSlider";
 import FormControl from "./components/FormControl";
+import {
+  NotificationContainer,
+  NotificationManager,
+} from "react-notifications";
 
 import Title from "./components/Title";
 import Password from "./components/Password";
@@ -25,23 +29,47 @@ function App() {
   const copyPassword = async (password) => {
     try {
       await navigator.clipboard.writeText(password);
-      toast({
-        title: "Copied!",
-        description: "Password copied to clipboard.",
-        status: "success",
-        duration: 2000,
-        isClosable: true,
-      });
-    } catch (error) {
-      toast({
-        title: "Error!",
-        description: "Could not copy password to clipboard.",
-        status: "error",
-        duration: 5000,
-        isClosable: true,
-      });
+      NotificationManager.success("Success message", "Title here");
+    } catch (err) {
+      console.error("error");
     }
   };
+
+  const generatePassword = () => {
+    if (!uppercase && !lowercase && !numbers && !symbols) {
+      alert("Please select at least one character type");
+    }
+
+    const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const lower = "abcdefghijqlmnopqrstuvwxyz";
+    const number = "123456789";
+    const symbol = "!@#$%^&*()+";
+
+    let userSelection = "";
+    let generatedPassword = "";
+
+    if (uppercase) {
+      userSelection += upper;
+    }
+    if (lowercase) {
+      userSelection += lower;
+    }
+    if (numbers) {
+      userSelection += number;
+    }
+    if (symbols) {
+      userSelection += symbol;
+    }
+
+    for (let i = 0; i < passwordLength; i++) {
+      generatedPassword += userSelection.charAt(
+        Math.floor(Math.random() * userSelection.length)
+      );
+    }
+
+    setPassword(generatedPassword);
+  };
+
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="space-y-2">
@@ -49,7 +77,7 @@ function App() {
         <Password
           password={password}
           setPassword={setPassword}
-          copyPassword={copyPassword}
+          onClick={copyPassword}
         />
         <RangeSlider
           passwordLength={passwordLength}
@@ -66,6 +94,13 @@ function App() {
           symbols={symbols}
           setSymbols={setSymbols}
         />
+        <button
+          type="button"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          onClick={generatePassword}
+        >
+          Create
+        </button>
       </div>
     </div>
   );
